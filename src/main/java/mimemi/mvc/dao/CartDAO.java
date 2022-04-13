7ece79d4050c165292391deb0bebbae9f72d6ee5
이아리 <71698417+LEEAHRI@users.noreply.github.com>
2022-04-13 22:15:20 +0900

    Cart,Order 인터페이스 수정 추

--- src/main/java/mimemi/mvc/dao/CartDAO.java
+++ src/main/java/mimemi/mvc/dao/CartDAO.java
@@ -1,16 +1,42 @@
 package mimemi.mvc.dao;
 
+import java.sql.SQLException;
+import java.util.List;
+
 import mimemi.mvc.dto.CartDTO;
+import mimemi.mvc.dto.OrderDeliDTO;
 
 public interface CartDAO {
 	
 	/**
-	 * 二쇰Ц - 二쇰Ц �긽�꽭�� 媛숈씠 �옣諛붽뎄�땲�� �긽�뭹�룄 �떎�� �떎 愿�怨꾧� �씠猷⑥뼱吏꾨떎.
-	 * 洹몄뿉 �뵲�씪 "二쇰Ц"-"二쇰Ц �긽�꽭"�쓽 愿�怨꾩� 媛숈씠 "�옣諛붽뎄�땲"-"�옣諛붽뎄�땲 �긽�꽭" 愿�怨꾧� �릺�뼱�빞 �븿.
-	 * "�옣諛붽뎄�땲 �긽�꽭" �뀒�씠釉� 異붽� �븘�슂가나다라
-	 * @param cartId
-	 * @param GoodsId
+	 * 장바구니 조회
+	 * @param CartId
+	 * @return
+	 * @throws SQLException
+	 */
+	List<CartDTO> selectByCartId(int cartId) throws SQLException;
+
+	/**
+	 * 장바구니 수정
+	 * @param: 
+	 * @return: 
+	 * */
+	int updateCart(CartDTO cart) throws SQLException;	
+	
+	/**
+	 * 장바구니 추가하기
+	 * @param 
+	 * @param cart
 	 * @return
 	 */
-	int insert(CartDTO cart);
+	
+	int insert(CartDTO cart) throws SQLException;
+	
+	/**
+	 * 장바구니 삭제하기
+	 * @param cartId
+	 * @return
+	 * @throws SQLException
+	 */
+	int deleteCart(int cartId) throws SQLException;
 }
