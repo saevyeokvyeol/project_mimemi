b468a01b670089f5d276b466f3f1fab69d48e29d
yuda <yudajgim@gmail.com>
2022-04-14 22:58:43 +0900

    카트, 오더 DAO 추가 및 수정, 장바구니 등록 메소드 생성, 장바구니 관련 메소드 추가, 카트, 오더 DAO 임플리먼트 파일 생성

--- src/main/java/mimemi/mvc/dao/CartDAOImpl.java
+++ src/main/java/mimemi/mvc/dao/CartDAOImpl.java
@@ -1,34 +1,126 @@
 package mimemi.mvc.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Properties;
 
 import mimemi.mvc.dto.CartDTO;
+import mimemi.mvc.util.DbUtil;
 
 public class CartDAOImpl implements CartDAO {
-
-	@Override
-	public List<CartDTO> selectByCartId(String userId) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+	private Properties proFile = new Properties();
+	
+	/**
+	 * dbQuery.properties 로딩해 Properties 객체에 저장
+	 * */
+	public CartDAOImpl() {
+		try {
+			proFile.load(getClass().getClassLoader().getResourceAsStream("dbQuery.properties"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-
-	@Override
-	public int updateCart(CartDTO cart) throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
+	
+	/**
+	 * 장바구니 등록
+	 * @param CartDTO(String userId, String goodsId, int cartQty, String cartWeekday, String cartPeriod,
+			String cartStart)
+	 * @return int(등록한 레코드 수)
+	 */
 	@Override
 	public int insert(CartDTO cart) throws SQLException {
+		Connection con = null;
+		PreparedStatement ps = null;
+		
+		String sql = proFile.getProperty("cart.insert");
+		// insert into cart values(CART_ID_SEQ.NEXTVAL, ?, ?, ?, ?, ?, ?);
+		int result = 0;
+		try {
+			con = DbUtil.getConnection();
+			ps = con.prepareStatement(sql);
+			ps.setString(1, cart.getUserId());
+			ps.setString(2, cart.getUserId());
+			ps.setString(3, cart.getUserId());
+			ps.setString(4, cart.getUserId());
+			ps.setString(5, cart.getUserId());
+			ps.setString(6, cart.getUserId());
+			
+			result = ps.executeUpdate();
+		} finally {
+			DbUtil.dbClose(ps, con);
+		}
+		return result;
+	}
+
+	/**
+	 * 장바구니 수량 변경
+	 * @param: int cartId, int cartQty
+	 * @return: int(수정한 레코드 수)
+	 * */
+	@Override
+	public int updateCartQty(int cartId, int cartQty) throws SQLException {
+		Connection con = null;
+		PreparedStatement ps = null;
+		
+		String sql = proFile.getProperty("");
+		return 0;
+	}
+
+	/**
+	 * 장바구니 수량 변경
+	 * @param: int cartId, String cartWeekday
+	 * @return: int(수정한 레코드 수)
+	 * */
+	@Override
+	public int updateCartWeekday(int cartId, String cartWeekday) throws SQLException {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
+	/**
+	 * 장바구니 수량 변경
+	 * @param: int cartId, String cartPeriod
+	 * @return: int(수정한 레코드 수)
+	 * */
 	@Override
-	public int deleteCart(int cartId) throws SQLException {
+	public int updateCartPeriod(int cartId, String cartPeriod) throws SQLException {
 		// TODO Auto-generated method stub
 		return 0;
+	}
+
+	/**
+	 * 장바구니 부분 삭제
+	 * @param int[] cartId
+	 * @return int(삭제한 레코드 수)
+	 */
+	@Override
+	public int deleteSelectedCart(int[] cartId) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/**
+	 * 장바구니 전체 삭제
+	 * @param String userId
+	 * @return int(삭제한 레코드 수)
+	 */
+	@Override
+	public int deleteAllCart(String userId) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/**
+	 * 장바구니 조회
+	 * @param userId(유저 아이디가 들어간 모든 장바구니 가져오기)
+	 * @return List<CartDTO>
+	 */
+	@Override
+	public List<CartDTO> selectCartByUserId(String userId) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
--- src/main/java/mimemi/mvc/dao/OrderDAO.java
+++ src/main/java/mimemi/mvc/dao/OrderDAO.java
@@ -1,32 +1,78 @@
 package mimemi.mvc.dao;
 
+import java.sql.Connection;
 import java.sql.SQLException;
-
-import mimemi.mvc.dto.OrderDTO;
-import mimemi.mvc.dto.OrderDeliDTO;
-import mimemi.mvc.dto.OrderLineDTO;
-
 import java.util.List;
+
+import mimemi.mvc.dto.AddrDTO;
+import mimemi.mvc.dto.OrderDTO;
+import mimemi.mvc.dto.OrderLineDTO;
 
 public interface OrderDAO {
 	/**
 	 * 주문 등록
 	 * 
 	 * 0. OrderDTO 안의 OrderLineDTO를 통해 상품의 총 구매 금액 구하기
-	 * 1. 새 주소로 구매한 경우 addr insert
-	 * 2. 주문 테이블 insert
+	 * 1. 주문 테이블 insert
+	 * 2. 새 주소로 구매한 경우 addr insert
 	 * 3. 적립금을 사용한 경우 회원 테이블에서 적립금 차감(update)
 	 * 4. 회원 테이블에 적립금 1% 적립(update)
 	 * 5. 주문 상세 insert
 	 * 6. 쿠폰 사용한 경우 쿠폰 사용 여부 변경(update)
+	 * 7. 장바구니에 담겨있었을 경우 장바구니 부분 삭제
 	 * 
-	 * 위의 모든 걸 
+	 * 위의 모든 걸 트랜잭션으로 처리
 	 * 
 	 * @param OrderDTO(String userId, int addrId, String payMethod, int payPoint, String orderMemo,
 	 * 			String takeMethod, String doorPwd, String usercouId)
 	 * @return int(등록된 데이터 수)
 	 */
 	int insertOrder(OrderDTO order) throws SQLException;
+	
+	/**
+	 * 2. 새 주소로 구매한 경우 addr insert
+	 * @param AddrDTO(String userId, String addrName, int zipcode, String addrAddr, String addrDetailAddr,
+				String receiverName, String receiverPhone)
+	 * @return int(등록된 데이터 수)
+	 * */
+	int insertAddr(Connection con, AddrDTO addr) throws SQLException;
+	
+	/**
+	 * 3. 적립금을 사용한 경우 회원 테이블에서 적립금 차감(update)
+	 * @param String userId, int usedPoint
+	 * @return int(수정된 데이터 수)
+	 * */
+	int decreamentUserPoint(Connection con, String userId, int usedPoint) throws SQLException;
+	
+	/**
+	 * 4. 회원 테이블에 적립금 1% 적립(update)
+	 * @param String userId, int addedPoint
+	 * @return int(수정된 데이터 수)
+	 * */
+	int increamentUserPoint(Connection con, String userId, int addedPoint) throws SQLException;
+	
+	/**
+	 * 5. 주문 상세 insert
+	 * @param OrderLineDTO(int orderId, String goodsId, int orderQty, int priceQty, String deliWeekday,
+	 *			String deliPeriod, String deliStart)
+	 * @return int(등록된 데이터 수)
+	 * */
+	int insertOrderLine(Connection con, OrderLineDTO orderLine) throws SQLException;
+	
+	/**
+	 * 6. 쿠폰 사용한 경우 쿠폰 사용 여부 변경(update)
+	 * @param OrderDTO(String userId, int addrId, String payMethod, int payPoint, String orderMemo,
+	 * 			String takeMethod, String doorPwd, String usercouId)
+	 * @return int(수정된 데이터 수)
+	 * */
+	int updateUserCoupon(Connection con, int userCouId) throws SQLException;
+	
+	/**
+	 * 7. 장바구니에 담겨있었을 경우 해당 장바구니 삭제
+	 * @param int userId, String goodsId
+	 * @return int(삭제한 레코드 수)
+	 * */
+	int deleteSelectedCart(Connection con, int userId, String goodsId) throws SQLException;
 	
 	/**
 	 * 주문 취소
