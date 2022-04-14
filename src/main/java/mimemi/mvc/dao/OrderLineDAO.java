43fb3306d10f041058187af67040713efc453105
이아리 <71698417+LEEAHRI@users.noreply.github.com>
2022-04-14 09:34:39 +0900

    test

--- src/main/java/mimemi/mvc/dao/OrderLineDAO.java
+++ src/main/java/mimemi/mvc/dao/OrderLineDAO.java
@@ -3,29 +3,35 @@ package mimemi.mvc.dao;
 import java.sql.SQLException;
 import java.util.List;
 
+import mimemi.mvc.dto.OrderDeliDTO;
 import mimemi.mvc.dto.OrderLineDTO;
 
 public interface OrderLineDAO {
 
 	/**
-	 * 二쇰Ц �긽�꽭 議고쉶
+	 *주문상세 조회
 	 * @return
 	 * @throws SQLException
 	 */
 	List<OrderLineDTO> selectByOrderId(int orderId) throws SQLException;
 	
 	/**
-	 * 二쇰Ц �긽�꽭 異붽�
+	 * 주문상세 추가
 	 * @param orderLine
 	 * @return
 	 */
 	int insert(OrderLineDTO orderLine) throws SQLException;
 	
 	/**
-	 * 二쇰Ц �긽�꽭 痍⑥냼
+	 * 주문상세 삭제
 	 * @param orderLineId
 	 * @return
 	 * @throws SQLException
 	 */
 	int delete(int orderLineId) throws SQLException;
+	
+	/**
+	 * 주문상세 수정
+	 */
+	int updatOrderLine (OrderLineDTO orderLine) throws SQLException;
 }
