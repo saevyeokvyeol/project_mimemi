cd3190fbec1960c3a4e0fc8a0396c65ab52e5b16
이아리 <71698417+LEEAHRI@users.noreply.github.com>
2022-04-19 17:13:00 +0900

    오류수정

---
+++ src/main/java/mimemi/mvc/controller/MealController.java
@@ -0,0 +1,14 @@
+package mimemi.mvc.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MealController implements Controller {
+
+	@Override
+	public ModelAndView hendlerRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
